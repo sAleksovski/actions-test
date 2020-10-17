24968f41839f92f4baa793d78c08aed48d06e84f
sAleksovski <aleksovski.stefan994@gmail.com>
2020-10-17 20:41:56 +0000

    Deploying to gh-pages from  @ 80f75a9813ed721069b2661f5c71803e943e6800 🚀

---
+++ precache-manifest.9e6d2d9667cdbd09972e652310ba9fb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a10504db4072119eead99f0b5ee1ba1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2d2fdd59c02a6bb9d43",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "51f61f9f61debdf42629",
+    "url": "/static/js/2.f1cf3dfd.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f1cf3dfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2d2fdd59c02a6bb9d43",
+    "url": "/static/js/main.73dbb4b3.chunk.js"
+  },
+  {
+    "revision": "dfa98899859338641b10",
+    "url": "/static/js/runtime-main.a03ee85d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
